d1f1489b45b9489ac0443da7356be1a53fe6d29b
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-05-03 12:23:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,29 +7,30 @@ while True:
 [2] Gerador de comentario''')
   menu = input('Escolha a opcao desejada :')
   os.system('clear')
-  if menu == '1'
+  if menu == '1':
     print('GERADOR DE EMAIL')
     print('''[1] Email inicial
 [2] Email de conclusao''')
     email = input('Escolha a opcao desejada :')
     os.system('clear')
-    print('EMAIL INICIAL')
-    gdi = input('Digite o numero da gdi: ')
-    razao_social = input('Digite a razao social do cliente: ')
-    cnpj = input('Digite o CNPJ matriz do cliente: ')
-    nome_responsavel = input('Digite o nome do responsavel: ')
-    ramal = input('Digite o seu ramal: ')
-    caixa_postal = input('Digite a caixa postal do cliente: ')
-    senha = input('Digite a senha do cliente: ')
-    os.system('clear')
-    print(f'''Assunto: GDI-{gdi} - {razao_social} - {cnpj}
+    if email == '1'
+      print('EMAIL INICIAL')
+      gdi = input('Digite o numero da gdi: ')
+      razao_social = input('Digite a razao social do cliente: ')
+      cnpj = input('Digite o CNPJ matriz do cliente: ')
+      nome_responsavel = input('Digite o nome do responsavel: ')
+      ramal = input('Digite o seu ramal: ')
+      caixa_postal = input('Digite a caixa postal do cliente: ')
+      senha = input('Digite a senha do cliente: ')
+      os.system('clear')
+      print(f'''Assunto: GDI-{gdi} - {razao_social} - {cnpj}
 
 Prezados, bom dia!
 
 Seguem informações importantes:
 
 Cliente:
-• Operadora responsável pela implantação será {nome_responsavel} no telefone (48) 2106.{ramal] ou (48) 2106.5741, e-mail implantacao.cartoes@nexxera.com
+• Operadora responsável pela implantação será {nome_responsavel} no telefone (48) 2106.{ramal} ou (48) 2106.5741, e-mail implantacao.cartoes@nexxera.com
 • Estamos aguardando o Documento de Implantação (em anexo) do cliente preenchido conforme orientações do kick-off.
 • Site da implantação: https://implantacaoalpha7.nexxera.com/
 

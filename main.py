ed7d6c4ce7978ea85d297d95378e0e295a4342f6
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-05-08 14:21:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,9 @@
 import re
 import os
 os.system('cls')
+
 # Texto fornecido
-texto = '''Prezados, boa tarde!
-
-Kick Off realizado com o cliente ERIC MARCELO M B VIEIRA, no dia 30/04/2024, pela Implantação Nexxera.
-Segue abaixo as informações que levantamos para o início da implantação:
-Bancos que serão utilizados: Sicredi, serviços: Conciliação, Pagamento, DDA, Cobrança.
-Para a demanda de Cartões: Redecard, 1 CNPJ/Ponto de Venda. Para a demanda de Cartões: Cielo, 2 CNPJ/Ponto de Venda.
-Contato chave / ponto focal do projeto: Maria (67)99962-7326
-Seguem informações importantes:
-
-Cliente:
-
-• Operador responsável pela implantação será Matheus Silvano no telefone (48)21065255 - ou setor (48)21065218, e-mail implantacao.cartoes@nexxera.com
-
-Alpha7:
-
-• Cliente CNPJ: 08.107.186/0001-94
-• Caixa postal: UPAMAMBAI.UPAMAMBAI
-• Senha: Upamambai08107186
-
-Qualquer dúvida estamos à disposição.'''
+texto = input('Insira o comentário pós kick-off: ')
 
 # Expressões regulares para extrair informações específicas
 
@@ -32,10 +14,11 @@ servicos_info = bancos_regex.group(2).strip().split(', ')
 operadoras_regex = re.findall(r'Para a demanda de Cartões: (.*?),', texto)
 operadoras_info = [operadora.strip() for operadora in operadoras_regex]
 
-cnpj_regex = re.search(r'Cliente CNPJ: (.*?)$', texto, re.MULTILINE)
+cnpj_regex = re.search(r'Cliente CNPJ: (.*?)[•]', texto, re.MULTILINE)
 cnpj_info = cnpj_regex.group(1).strip() if cnpj_regex else None
 
 # RESUMO TÉCNICO
+os.system('cls')
 operadoras_str = ', '.join(operadoras_info) if len(operadoras_info) > 1 else operadoras_info[0]
 bancos_str = ', '.join(bancos_info) if len(bancos_info) > 1 else bancos_info[0]
 print(f'''{{panel:title=RESUMO TÉCNICO|borderStyle=dashed|borderColor=#000000|titleBGColor=#97acd1|bgColor=#e0e6ff}}
@@ -51,9 +34,10 @@ Bancos: {bancos_str}| |
 # OPERADORAS E CNPJ
 print('||OPERADORA||CNPJ||EC||STATUS||AÇÃO||')
 for operadora in operadoras_info:
-    print('''|{operadora}|{cnpj}|(x)|(x)|Aguardando DI preenchido|'''.format(operadora=operadora, cnpj=cnpj_info))
+    print(f'''|{operadora}|{cnpj_info}|(x)|(x)|Aguardando DI preenchido|''')
 
 # BANCOS
 print('''||BANCO||CNPJ||SERVIÇO||CONTA||AGÊNCIA||STATUS||AÇÃO||''')
-for servico in servicos_info:
-    print('|SICREDI|08.107.186/0001-94|{servico}|(x)|(x)|(x)|Aguardando relacionamento|'.format(servico=servico))
+for banco in bancos_info:
+    for servico in servicos_info:
+        print(f'|{banco}|{cnpj_info}|{servico}|(x)|(x)|(x)|Aguardando relacionamento|')
